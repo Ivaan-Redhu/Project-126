3e0004b888c5f6dcf2f080660684e18c77b6829d
Ivaan-Redhu <93902463+Ivaan-Redhu@users.noreply.github.com>
2023-08-08 19:12:23 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song_adele = "";
 song_aurora = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -13,8 +17,29 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);    
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Intialized')
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY)
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY)
+    }
 }
