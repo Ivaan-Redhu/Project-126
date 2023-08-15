976dd47d78625391727f72cac6c693e7f28f401a
Ivaan-Redhu <93902463+Ivaan-Redhu@users.noreply.github.com>
2023-08-15 17:58:43 +1200

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 song_adele = "";
 song_aurora = "";
+song_adele_status = "";
+song_aurora_status = "";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
 
 function preload()
 {
-    song = loadSound("adele.mp3");
-    song = loadSound("aurora.mp3");
+    song_adele = loadSound("adele.mp3");
+    song_aurora = loadSound("aurora.mp3");
 }
 
 function setup() {
@@ -23,7 +26,23 @@ function setup() {
 }
 
 function draw() {
-    image(video, 0, 0, 600, 500);    
+    image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    song_adele_status = song_adele.isPlaying();
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX,leftWristY,20)
+        song_aurora.stop();
+        if(song_adele_status == false){
+            song_adele.play();
+        }
+        else{
+            document.getElementById("song_id").innerHTML = "Song Name: Oh My God - Adele";
+        }
+    }
 }
 
 function modelLoaded() {
@@ -34,6 +53,9 @@ function gotPoses(results)
 {
     if(results.length > 0)
     {
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY)
@@ -42,4 +64,8 @@ function gotPoses(results)
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY)
     }
+}
+
+function isPlaying() {
+
 }
